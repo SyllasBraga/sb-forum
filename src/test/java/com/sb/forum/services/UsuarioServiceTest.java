73f4de8025c57cc4bbb4bb3c965b3fe29a448ccd
Syllas Braga <syllasbraga2@gmail.com>
2023-03-02 18:16:06 -0300

    feat: Classe de testes para UsuarioService

---
+++ src/test/java/com/sb/forum/services/UsuarioServiceTest.java
@@ -0,0 +1,53 @@
+package com.sb.forum.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void createTopico() {
+    }
+
+    @Test
+    void updateTopico() {
+    }
+
+    @Test
+    void deleteTopico() {
+    }
+
+    @Test
+    void toUsuarioDto() {
+    }
+
+    @Test
+    void toUsuario() {
+    }
+}
