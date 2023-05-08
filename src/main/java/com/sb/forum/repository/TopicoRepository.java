a8634a35c9cabfe286c32b7d6be6bf1dfe860346
Syllas Braga <syllasbraga2@gmail.com>
2023-05-08 12:14:04 -0300

    feat(TopicoRepository): Bucas por palavras-chave presentes no conteudo do tópico

--- src/main/java/com/sb/forum/repository/TopicoRepository.java
+++ src/main/java/com/sb/forum/repository/TopicoRepository.java
@@ -4,6 +4,10 @@ import com.sb.forum.entities.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    List<Topico> FindByConteudoLike (String palavrasChave);
 }
