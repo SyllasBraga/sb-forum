23c8eb34112e02278b4b00399b36f9a7bfd30afe
Syllas Braga <syllasbraga2@gmail.com>
2023-06-15 17:42:29 -0300

    refactor: Adicionando um pacote exclusivo para as classes de autenticação

--- src/main/java/com/sb/forum/config/security/JwtConfig.java
+++ src/main/java/com/sb/forum/config/security/JwtConfig.java
@@ -1,4 +1,4 @@
-package com.sb.forum.config;
+package com.sb.forum.config.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
