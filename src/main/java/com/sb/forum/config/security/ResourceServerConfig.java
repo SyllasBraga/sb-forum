a648b4dc1a9db3d9ec9cfffa1fa70aad22447636
Syllas Braga <syllasbraga2@gmail.com>
2023-06-16 16:20:26 -0300

    feat(ResourceServerConfig): Implementação da classe que lê os tokens e direciona as rotas dos usuários

---
+++ src/main/java/com/sb/forum/config/security/ResourceServerConfig.java
@@ -0,0 +1,38 @@
+package com.sb.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    private final JwtTokenStore tokenStore;
+    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] USUARIO_OU_MODERADOR = {"/topicos/**"};
+    private static final String[] MODERADOR = {"/usuarios/**"};
+
+    public ResourceServerConfig(JwtTokenStore tokenStore) {
+        this.tokenStore = tokenStore;
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll()
+                .antMatchers(HttpMethod.GET, USUARIO_OU_MODERADOR).permitAll()
+                .antMatchers(USUARIO_OU_MODERADOR).hasAnyRole("user", "moderator")
+                .antMatchers(MODERADOR).hasRole("moderator")
+                .anyRequest().authenticated();
+    }
+}
