b9fabbf08b6404d357a774a63c3d2d7cd546d9cd
Syllas Braga <syllasbraga2@gmail.com>
2023-06-23 11:08:42 -0300

    refactor: Versão do Swagger e suas configurações iniciais

--- src/main/java/com/sb/forum/config/security/ResourceServerConfig.java
+++ src/main/java/com/sb/forum/config/security/ResourceServerConfig.java
@@ -13,7 +13,8 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     private final JwtTokenStore tokenStore;
-    private static final String[] PUBLIC = {"/oauth/token"};
+    private static final String[] PUBLIC = {"/oauth/token", "/", "/swagger-ui.html/**", "/webjars/**",
+            "/swagger-resources/**", "/v2/**", "/csrf/**"};
     private static final String[] USUARIO_OU_MODERADOR = {"topicos/**"};
     private static final String[] MODERADOR = {"usuarios/**"};
 
@@ -28,6 +29,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        http.headers().frameOptions().disable();
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 .antMatchers(HttpMethod.GET, USUARIO_OU_MODERADOR).permitAll()
