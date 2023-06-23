2258f86a5c1bed42c7f14493b4a8dd2a8ff109b1
Syllas Braga <syllasbraga2@gmail.com>
2023-06-23 11:39:09 -0300

    feat(SwaggerConfig): Configurações para o Swagger

--- src/main/java/com/sb/forum/config/SwaggerConfig.java
+++ src/main/java/com/sb/forum/config/SwaggerConfig.java
@@ -2,11 +2,13 @@ package com.sb.forum.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @Configuration
 @EnableSwagger2
@@ -15,7 +17,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .apis(RequestHandlerSelectors.basePackage(""))
                 .paths(PathSelectors.any())
                 .build();
     }
